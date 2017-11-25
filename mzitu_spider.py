f847fabbd900f1f0dc0458ca29dea5eb88a6ef9d
Yowe <ruwangxiansheng@sina.com>
2017-11-25 14:37:16 +0800

    修复中文乱码

--- mzitu_spider.py
+++ mzitu_spider.py
@@ -1,39 +1,85 @@
-#-*- coding:utf-8 -*-
-from bs4 import BeautifulSoup
+# coding=utf-8
+
+
 import os
-from DownLoad import request
+import requests
+import re
+import random
+import time
+from bs4 import BeautifulSoup
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-class mzitu():
+
+
+class MziTu:
+
+    def __init__(self):
+        self.name = 'mzitu'
+        self.iplist = []
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36'}
+        html = requests.get('http://www.xicidaili.com/', headers=headers)
+        pattern = re.compile(
+            r"((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))")
+
+        iplistn = pattern.findall(html.text)
+        ips1 = [g[0] for g in iplistn]
+        for ip in ips1:
+            self.iplist.append(ip.strip())
+
+        self.user_agent_list = [
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+        ]
+
+
+
     def load_all_url(self, url):
-        html = request.get(url, 3)
+        html = self.get(url, 3)
         all_a = BeautifulSoup(html.text, 'lxml').find('div', class_='all').find_all('a')
         for a in all_a:
             title = a.get_text()
-            print "开始保存:",title
-            path = str(title).strip().decode('utf-8')
+            print "开始保存:", str(title).strip()
+
+            path = str(title).strip().replace(':', '').decode('utf-8')
             self.mkdir(path)
+
             href = a['href']
-            self.html(href)  #套图的地址
+            self.html(href)
 
     def html(self, href):
-        html = request.get(href, 3)
+        html = self.get(href, 3)
         max_span = BeautifulSoup(html.text, 'lxml').find('div', class_='pagenavi').find_all('span')[-2].get_text()
         for page in range(1, int(max_span)+1):
             page_url = href + '/' + str(page)
             self.img(page_url)
 
     def img(self, page_url):
-        img_html = request.get(page_url, 3)
+        img_html = self.get(page_url, 3)
         img_url = BeautifulSoup(img_html.text, 'lxml').find('div', class_='main-image').find('img')['src']
         self.save_file(img_url)
 
     #保存图片
     def save_file(self, img_url):
         name = img_url[-9:-4]
-        img = request.get(img_url, 3)
+        img = self.get(img_url, 3)
         f = open(name + '.jpg', 'ab')
         f.write(img.content)
         f.close()
@@ -41,18 +87,51 @@ class mzitu():
 
     #创建文件夹
     def mkdir(self, path):
+
         dirPath = os.path.join("D:\mzitu", path)
         isExists = os.path.exists(dirPath)
         if not isExists:
-            os.mkdir(dirPath)
+            os.makedirs(dirPath)
             os.chdir(dirPath)
             return True
         else:
             return False
 
+    def get(self, url, timeout, proxy=None, num_retries=6):
+        ua = random.choice(self.user_agent_list)
+        headers = {'User-Agent': ua,'Referer':'http://www.mzitu.com/99566'}
+        if proxy == None:
+            try:
+                return requests.get(url, headers=headers,timeout=timeout)
+            except:
+                if num_retries > 0:
+                    time.sleep(10)
+                    print(u'获取网页出错，10S后将获取倒数第：', num_retries, u'次')
+                    return self.get(url, timeout, proxy,num_retries-1)
+                else:
+                    print (u'开始使用代理')
+                    time.sleep(10)
+                    IP = ''.join(str(random.choice(self.iplist)).strip())  #下面有解释哦
+                    proxy = {'http': IP}
+                    return self.get(url, timeout, proxy, )  #代理不为空的时候
+        else:
+            try:
+                ip = ''.join(str(random.choice(self.iplist)))
+                proxy = {'http':ip}
+                return requests.get(url, headers=headers, proxies=proxy, timeout=timeout)
+            except:
+
+                if num_retries > 0:
+                    time.sleep(10)
+                    IP = ''.join(str(random.choice(self.iplist)).strip())
+                    proxy = {'http': IP}
+                    print(u'正在更换代理，10S后将重新获取倒数第', num_retries, u'次')
+                    print(u'当前代理是：', proxy)
+                    return self.get(url, timeout, proxy, num_retries - 1)
+                else:
+                    print(u'代理也不好使了！取消代理')
+                    return self.get(url, 3)
 
 
-
-
-mzspider = mzitu()
-mzspider.load_all_url('http://www.mzitu.com/all')
+mz = MziTu()
+mz.load_all_url('http://www.mzitu.com/all')
