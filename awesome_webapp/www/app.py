2fda2af3fda2a4802f4ab779ec5de534999a395e
yowe <763797173@qq.com>
2017-12-19 16:23:29 +0800

    update app.py

--- awesome_webapp/www/app.py
+++ awesome_webapp/www/app.py
@@ -2,3 +2,25 @@
 # -*- coding: utf-8 -*-
 
 __author__="Yowe"
+
+'''
+async web application
+'''
+
+import  logging;logging.basicConfig(level=logging.INFO)
+import asyncio,os,json,time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+def init(loop):
+    app=web.application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
