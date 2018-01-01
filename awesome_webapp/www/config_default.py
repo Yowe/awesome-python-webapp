f587f13137e02cd3bd91023890dcc6b6b694fec7
Yowe <ruwangxiansheng@sina.com>
2018-01-01 20:24:09 +0800

    add mvc

---
+++ awesome_webapp/www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'YOWE'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '123456',
+        'db': 'awesome'
+    },
+    'session': 'Awesome'
+           }
+
