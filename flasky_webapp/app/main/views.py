7bd2c40abb92fd0e9f9eeea6a5c588b27dd3fe5a
Yowe <ruwangxiansheng@sina.com>
2018-01-21 18:14:35 +0800

    add profile

--- flasky_webapp/app/main/views.py
+++ flasky_webapp/app/main/views.py
@@ -3,9 +3,10 @@ from flask import render_template, session, redirect, url_for, abort, flash
 from flask_login import login_required, current_user
 
 from . import main
-from .forms import NameForm, EditProfileForm
-from ..models import User
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
+from ..models import User, Role
 from .. import db
+from ..decorators import admin_required
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -14,7 +15,7 @@ def index():
     if form.validate_on_submit():
         return redirect(url_for('.index'))
     
-    return render_template('main/index.html',
+    return render_template('index.html',
                            form=form,
                            name=session.get('name'),
                            known=session.get('konwn', False),
@@ -37,11 +38,40 @@ def edit_profile():
         current_user.name = form.name.data
         current_user.location = form.location.data
         current_user.about_me = form.about_me.data
-        db.session.add(user)
+        db.session.add(current_user)
+        db.session.commit()
         flash('Your Profile has been updated.')
-        return redirect(url_for('.user',username=current_user.username))
+        return redirect(url_for('.user', username=current_user.username))
 
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+
+@main.route('/wdit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me
+        db.session.add(user)
+        db.session.commit()
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
--- flasky_webapp/app/models.py
+++ flasky_webapp/app/models.py
@@ -105,7 +105,7 @@ class User(UserMixin, db.Model):
             return False
         if data.get('confirm') != self.id:
             return False
-        self.confirmed=True
+        self.confirmed = True
         db.session.add(self)
         return True
 
